72a849c809558bf443d966dec8551a5509476c1d
Vuongdaneil <vuong.dm19010044@st.phenikaa-uni.edu.vn>
2022-03-13 13:26:10 +0700

    update: connect back-end with front-end, login fb, gg, etc...

--- Beanikaa/app/src/main/java/com/example/beanikaa/LoginScreen.java
+++ Beanikaa/app/src/main/java/com/example/beanikaa/LoginScreen.java
@@ -1,5 +1,7 @@
 package com.example.beanikaa;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -12,6 +14,7 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -26,18 +29,27 @@ import com.example.beanikaa.Service.SendMail;
 import com.example.beanikaa.common.Account;
 import com.example.beanikaa.data.Pojo.User;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.material.textfield.TextInputEditText;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
-public class LoginScreen extends AppCompatActivity {
+public class LoginScreen extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
     private static final String TAG = LoginScreen.class.getName();
     TextInputEditText Phone_field, Pass_field;
     Button loginbtn;
+    ImageButton fbLoginBtn, ggLoginBtn;
     ProgressBar progressBar;
-    TextView forgotpass;
+    TextView forgotpass, loginActBtn;
+    private GoogleApiClient googleApiClient;
+    private static final int SIGN_IN = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +67,12 @@ public class LoginScreen extends AppCompatActivity {
         progressBar = findViewById(R.id.progress);
         Phone_field = findViewById(R.id.phone_field);
         Pass_field = findViewById(R.id.password_field);
+        loginActBtn = findViewById(R.id.toLoginActBtn);
+
+        //Facebook & Gg buttons
+        fbLoginBtn = findViewById(R.id.facebookbtn);
+        ggLoginBtn = findViewById(R.id.googlebtn);
+
 
         Phone_field.setHint("Your phone number");
         Pass_field.setHint("Password");
@@ -78,15 +96,14 @@ public class LoginScreen extends AppCompatActivity {
 
 
         //xu ly du lieu
-
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String phonenumber, password;
-                phonenumber = String.valueOf(Phone_field.getText());
+                String email, password;
+                email = String.valueOf(Phone_field.getText());
                 password = String.valueOf(Pass_field.getText());
 
-                if (!phonenumber.equals("") && !password.equals("")){
+                if (!email.equals("") && !password.equals("")){
 
                     progressBar.setVisibility(View.VISIBLE);
 
@@ -97,19 +114,19 @@ public class LoginScreen extends AppCompatActivity {
                         //Starting Write and Read data with URL (url của localhost)
                         //Creating array for parameters
                         String[] field = new String[2];
-                        field[0] = "phoneNumber";
+                        field[0] = "email";
                         field[1] = "password";
 
                         //Creating array for data
                         String[] data = new String[2];
-                        data[0] = phonenumber;
+                        data[0] = email;
                         data[1] = password;
 
-<<<<<<< HEAD
+//<<<<<<< HEAD
                         PutData putData = new PutData("http://192.168.188.243//Beanikaa/login.php", "POST", field, data);
-=======
-                        PutData putData = new PutData("http://192.168.0.102/android/login", "POST", field, data);
->>>>>>> 021603ec38cd8d1f52762da787e0b082906bd764
+//=======
+//                        PutData putData = new PutData("http://192.168.0.102/android/login", "POST", field, data);
+//>>>>>>> 021603ec38cd8d1f52762da787e0b082906bd764
                         if (putData.startPut()) {
                             if (putData.onComplete()) {
                                 String result = putData.getResult();
@@ -151,6 +168,37 @@ public class LoginScreen extends AppCompatActivity {
             }
 
         });
+
+
+        //Facebook login
+        fbLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), facebook_login.class);
+                startActivity(intent);
+            }
+        });
+
+
+        //Google login
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        googleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this, this).addApi(Auth.GOOGLE_SIGN_IN_API, gso).build();
+        ggLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =  Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+                startActivityForResult(intent, SIGN_IN);
+            }
+        });
+
+        //Already had account, login
+        loginActBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SignUp.class);
+                startActivity(intent);
+            }
+        });
     }
     public void checkLogin(String url){
 
@@ -160,7 +208,6 @@ public class LoginScreen extends AppCompatActivity {
             public void onResponse(String response) {
 
                 Toast.makeText(getApplicationContext(),"Response :" + response.toString(), Toast.LENGTH_LONG).show();//display the response on screen
-
             }
         }, new Response.ErrorListener() {
 
@@ -172,5 +219,26 @@ public class LoginScreen extends AppCompatActivity {
         });
 
         mRequestQueue.add(mStringRequest);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult){
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == SIGN_IN){
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+
+            if(result.isSuccess()){
+                startActivity(new Intent(LoginScreen.this, MainActivity.class));
+                finish();
+            }
+            else{
+                Toast.makeText(this,"Login Failed", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
---
+++ Beanikaa/app/src/main/java/com/example/beanikaa/SignUp.java
@@ -0,0 +1,27 @@
+package com.example.beanikaa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SignUp extends AppCompatActivity {
+
+    EditText phone,email,password,confirmpassword;
+    Button exitbutton,registerbutton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+    private void Id_link(){
+        phone = (EditText)findViewById(R.id.phone);
+        password = (EditText)findViewById(R.id.password);
+        email = (EditText)findViewById(R.id.email);
+        confirmpassword = (EditText)findViewById(R.id.confirmpassword);
+        exitbutton = (Button)findViewById(R.id.exitbutton);
+        registerbutton = (Button)findViewById(R.id.registertbutton);
+    }
+}
