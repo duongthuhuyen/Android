1c4c8ca7ccaac516fb34e2c8ebd53cec7d0e649a
Huyne459 <96757832+Huyne459@users.noreply.github.com>
2022-04-23 21:18:48 +0700

    Delete htdocs directory

--- htdocs/Beanikaa/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-require "DataBase.php";
-$db = new DataBase();
-$email = "Huyen@email.com";
-$pass = "Aa123456@";
-if ($db->dbConnect()) {
-	echo json_encode($db->logIn("user", $email, $pass));
-}
-?>
